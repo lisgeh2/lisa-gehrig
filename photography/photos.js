02673ec77b703042b6cda2da9c80924ffabd3eba
Lisa Gehrig <lisa.gehrig2@uzh.ch>
2026-09-25 19:41:00 +0200

    correct img counts

--- photography/photos.js
+++ photography/photos.js
@@ -1,6 +1,6 @@
 // Number of photos in each folder.
 // Change these numbers to match your collections.
-const photoCounts = [7, 6, 6];
+const photoCounts = [7, 6, 7, 8, 4, 6, 4];
 
 const photos = photoCounts.map((count, sectionIndex) =>
   Array.from({ length: count }, (_, imageIndex) => {
